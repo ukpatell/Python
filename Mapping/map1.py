ffcee7c4d27ced0701fa26dee7e36ab405bca463
Umang <88688048+ukpatell@users.noreply.github.com>
2021-12-26 22:46:15 -0500

    Feed markers from Volcanoes.txt

--- Mapping/map1.py
+++ Mapping/map1.py
@@ -1,12 +1,25 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+# Import data file using pandas library in data object
+
+lat  = list(data["LAT"])
+lon  = list(data["LON"])
+elev = list(data["ELEV"])
+# convert data txt in list format for easy access
 
 map = folium.Map(location=[40.26, -76.88], zoom_start=4, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for co_ordinates in [[40.26,-76.88],[40.40,-95]]:
-    fg.add_child(folium.Marker(location=co_ordinates, popup = "Marker", icon=folium.Icon(color='green')))
-# You can also add multiple markers by using line #6
+# Loop iterates a list of co_ordinates
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup = el, icon=folium.Icon(color='green')))
+# NOTE: Unsure why el = default is float but it still run, without converting to string as expectation
+# Run: help(folium) --> a
+# You can also add multiple markers by using line 9 multiple times or using a Loop
+# In here, we have used zip to iterate loop at the same time from the imported file
 
 map.add_child(fg)
 
