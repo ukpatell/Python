a883f00e45a28a86e843b37996fdcc2954ed1783
ukpatell <umangpatelr4@gmail.com>
2022-02-16 00:20:32 -0500

    Data Entered Manually

--- Programs/Python/patel_searchtests.py
+++ Programs/Python/patel_searchtests.py
@@ -2,4 +2,22 @@
 # Date       : February 18, 2022
 # Instructor : Professor Eugene
 # Description: Run series of tests to illustrate the big-O efficiency
-#              of linear search and binary search
+#              of linear search and binary search via random numbers
+#              with & without duplicates from (1-100) inclusive
+# GitHub     : https://github.com/ukpatell/Python.git
+
+
+five = [78, 31, 1, 99, 42]  # 5 - Unique Numbers
+fiveD = [89, 89, 66, 7, 9]  # 5 - Numbers w. Duplicates
+ten = [10, 63, 17, 31, 82, 78, 24, 20, 57, 97]  # 10 - Unique Numbers
+tenD = [46, 2, 100, 46, 79, 90, 99, 68, 100, 50]  # 10 - Numbers w. Duplicates
+tfive = [7, 24, 36, 44, 60, 56, 21, 64, 46, 89, 99, 40, 96, 88, 98, 68, 83, 87, 86, 2, 72, 5, 27, 33,
+         28]  # 25 - Unique Numbers
+tfiveD = [25, 21, 99, 17, 39, 21, 77, 71, 79, 53, 35, 45, 53, 87, 19, 21, 37, 7, 43, 13, 43, 95, 89, 13,
+          7]  # 25 - Numbers w. Duplicates
+fifty = [49, 1, 37, 7, 92, 64, 59, 13, 57, 15, 36, 31, 84, 2, 60, 44, 77, 68, 25, 17, 94, 83, 58, 42, 10, 29,
+         12, 52, 8, 85, 38, 30, 41, 53, 24, 69, 91, 47, 43, 67, 55, 35, 75, 39, 34, 21, 28, 20, 46,
+         72]  # 50 - Unique Numbers
+fiftyD = [60, 12, 83, 1, 9, 80, 45, 86, 78, 80, 62, 37, 46, 96, 9, 49, 45, 13, 43, 22, 4, 29, 56, 60, 70, 9, 95,
+          62, 36, 75, 94, 96, 25, 17, 17, 10, 8, 76, 8, 19, 9, 32, 13, 89, 98, 9, 81, 88, 17,
+          17]  # 50 - Numbers w. Duplicates
