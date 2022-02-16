84990bced9fc535c6cec3698ed8941322979fd25
ukpatell <umangpatelr4@gmail.com>
2022-02-15 20:21:10 -0500

    Initial Program File

---
+++ Programs/Python/patel_searchtests.py
@@ -0,0 +1,5 @@
+# Name       : Umangkumar Patel
+# Date       : February 18, 2022
+# Instructor : Professor Eugene
+# Description: Run series of tests to illustrate the big-O efficiency
+#              of linear search and binary search
