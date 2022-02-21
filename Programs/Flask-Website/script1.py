5f7dd42eb06966b39d829e000da9f4918f9e10a7
ukpatell <umangpatelr4@gmail.com>
2022-02-21 18:40:07 -0500

    Basic Static Created

--- Programs/Flask-Website/script1.py
+++ Programs/Flask-Website/script1.py
@@ -1,14 +1,14 @@
-from flask import Flask # Import Flask object
+from flask import Flask,render_template,url_for # Import Flask object
 
 app = Flask(__name__)   # Instance of Flask
 
-@app.route('/home/')         # Route to Home
+@app.route('/')         # Route to Home
 def home():             # Home Function
-    return "Home Page"
+    return render_template('home.html')
 
 @app.route('/about/')
 def about(): # About Function
-    return "About Page"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
